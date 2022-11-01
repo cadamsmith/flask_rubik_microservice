03a71ad1f708760a0a98f082ce8d0ef0c69a592c
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-01 15:59:30 -0500

    Green Light |

--- rubik/cubeFace.py
+++ rubik/cubeFace.py
@@ -2,9 +2,9 @@
 from enum import Enum
 
 class CubeFace(Enum):
-    FRONT = 'f'
-    BACK = 'b'
-    LEFT = 'l'
-    RIGHT = 'r'
-    UP = 'u'
-    DOWN = 'd'
+    FRONT = 'F'
+    BACK = 'B'
+    LEFT = 'L'
+    RIGHT = 'R'
+    UP = 'U'
+    DOWN = 'D'
