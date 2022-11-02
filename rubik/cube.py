e8f82de338751af9f683d52e008965abce3595bb
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 07:36:18 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -1,14 +1,16 @@
 
 import re
+import itertools
 from rubik.cubeColor import CubeColor
+from rubik.cubelet import Cubelet
 
 class Cube:
-    SIZE = 3
-    CODE_LENGTH = 54
-    
     '''
     Rubik's cube
     '''
+
+    SIZE = 3
+    CODE_LENGTH = 54
 
     def __init__(self, cubeCode):
         # make sure supplied param is a string
@@ -40,3 +42,5 @@ class Cube:
         
         assert (len(centerColors) == len(CubeColor))
         
+        for i, j, k in itertools.product(*[range(Cube.SIZE)] * 3):
+            self.cubelets[i, j, k] = Cubelet()
