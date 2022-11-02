dd72acb9b1d2fa0a425a05ccd0c58b5f043b4150
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-01 19:03:42 -0500

    Red Light |

--- rubik/test/cubeletTest.py
+++ rubik/test/cubeletTest.py
@@ -9,13 +9,37 @@ class CubeletTest(TestCase):
     # __init__ - POSITIVE TESTS
     
     # there should be only 6 cube faces (by nature of a cube)
-    def test_cublet_init_10010_ShouldInstantiateCubeForValidInput(self):
+    def test_cubelet_init_10010_ShouldInstantiateCubeForValidInput(self):
     
-        sides = {
+        faces = {
             CubeFace.UP: CubeColor.RED,
             CubeFace.FRONT: CubeColor.BLUE,
             CubeFace.LEFT: CubeColor.WHITE
         }
         
-        cubelet = Cubelet(sides)
+        cubelet = Cubelet(faces)
         self.assertIsInstance(cubelet, Cubelet)
+        
+    # __init__ - NEGATIVE TESTS
+    
+    # supplying a dictionary with non-CubeFace keys should throw exception
+    def test_cubelet_init_20010_ShouldThrowExceptionForInvalidParamKeys(self):
+        
+        faces = {
+            1: CubeColor.GREEN,
+            True: CubeColor.WHITE
+        }
+        
+        with self.assertRaises(Exception):
+            Cubelet(faces)
+            
+    # supplying a dictionary with non-CubeColor values should throw exception
+    def test_cubelet_init_20020_ShouldThrowExceptionForInvalidParamValues(self):
+        
+        faces = {
+            CubeFace.BACK: 2.01,
+            CubeFace.RIGHT: 'value'
+        }
+        
+        with self.assertRaises(Exception):
+            Cubelet(faces)
