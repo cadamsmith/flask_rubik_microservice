f6bb57809e57278df2184f6d191b9b67246ed487
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 06:32:44 -0500

    Refactor |

--- rubik/cube.py
+++ rubik/cube.py
@@ -16,24 +16,24 @@ class Cube:
         elif isinstance(faces, dict):
             pass
         
-    def __initFromCubeCode__(self, facesCode):
+    def __initFromCubeCode__(self, cubeCode):
         # make sure supplied param is a string
-        assert (isinstance(facesCode, str))
+        assert (isinstance(cubeCode, str))
         
         # make sure "cube code" is 54 chars long
-        assert (len(facesCode) == 54)
+        assert (len(cubeCode) == 54)
         
         # make sure is over alphabet [brgoyw]
-        assert (not bool(re.search('[^brgoyw]', facesCode)))
+        assert (not bool(re.search('[^brgoyw]', cubeCode)))
         
         # make sure contains every color
         for color in list(CubeColor):
-            assert (facesCode.__contains__(color.value))
+            assert (cubeCode.__contains__(color.value))
             
         # make sure contains even distribution of colors
         colorDistributions = {c: 0 for c in CubeColor}
         
-        for letter in facesCode:
+        for letter in cubeCode:
             color = CubeColor(letter)
             colorDistributions[color] += 1
         
@@ -41,7 +41,7 @@ class Cube:
         
         # make sure there are no 2 center cubelet faces with same color
         centerColors = set(map(lambda letter: CubeColor(letter), [
-            facesCode[4], facesCode[13], facesCode[22], facesCode[31], facesCode[40], facesCode[49]
+            cubeCode[4], cubeCode[13], cubeCode[22], cubeCode[31], cubeCode[40], cubeCode[49]
         ]))
         
         assert (len(centerColors) == len(CubeColor))
