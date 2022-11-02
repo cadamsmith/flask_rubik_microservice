251bcd470bc0826d8b2b0280b543dc9f9aaa3bad
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 06:13:56 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -16,16 +16,25 @@ class Cube:
         elif isinstance(faces, dict):
             pass
         
-    def __initFromCubeCode__(self, faces):
+    def __initFromCubeCode__(self, facesCode):
         # make sure supplied param is a string
-        assert (isinstance(faces, str))
+        assert (isinstance(facesCode, str))
         
         # make sure "cube code" is 54 chars long
-        assert (len(faces) is 54)
+        assert (len(facesCode) is 54)
         
         # make sure is over alphabet [brgoyw]
-        assert (not bool(re.search('[^brgoyw]', faces)))
+        assert (not bool(re.search('[^brgoyw]', facesCode)))
         
         # make sure contains every color
         for color in list(CubeColor):
-            assert (faces.__contains__(color.value))
+            assert (facesCode.__contains__(color.value))
+            
+        # make sure contains even distribution of colors
+        colorDistributions = {c: 0 for c in CubeColor}
+        
+        for letter in facesCode:
+            color = CubeColor(letter)
+            colorDistributions[color] += 1
+        
+        assert (len(set(colorDistributions.values())) is 1)
