510a7cd52895086bcfaf1317b35aa66e606f22a3
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 10:25:16 -0500

    Refactor |

--- rubik/cube.py
+++ rubik/cube.py
@@ -3,6 +3,7 @@ import re
 import itertools
 from rubik.cubeColor import CubeColor
 from rubik.cubelet import Cubelet
+from rubik.cubeCode import CubeCode
 
 class Cube:
     '''
@@ -15,34 +16,8 @@ class Cube:
     cubelets = {}
 
     def __init__(self, cubeCode):
-        # make sure supplied param is a string
-        assert (isinstance(cubeCode, str))
-        
-        # make sure "cube code" is 54 chars long
-        assert (len(cubeCode) == Cube.CODE_LENGTH)
-        
-        # make sure is over alphabet [brgoyw]
-        assert (not bool(re.search('[^brgoyw]', cubeCode)))
-        
-        # make sure contains every color
-        for color in list(CubeColor):
-            assert (cubeCode.__contains__(color.value))
-            
-        # make sure contains even distribution of colors
-        colorDistributions = {c: 0 for c in CubeColor}
-        
-        for letter in cubeCode:
-            color = CubeColor(letter)
-            colorDistributions[color] += 1
-        
-        assert (len(set(colorDistributions.values())) == 1)
-        
-        # make sure there are no 2 center cubelet faces with same color
-        centerColors = set(map(lambda letter: CubeColor(letter), [
-            cubeCode[4], cubeCode[13], cubeCode[22], cubeCode[31], cubeCode[40], cubeCode[49]
-        ]))
-        
-        assert (len(centerColors) == len(CubeColor))
+        # make sure supplied param is a valid CubeCode
+        assert (isinstance(cubeCode, CubeCode))
         
         for i, j, k in itertools.product(*[range(Cube.SIZE)] * 3):
             self.cubelets[i, j, k] = Cubelet()
--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -3,6 +3,7 @@ import itertools
 from unittest import TestCase
 from rubik.cube import Cube
 from rubik.cubelet import Cubelet
+from rubik.cubeCode import CubeCode
 
 class CubeTest(TestCase):
     
@@ -10,75 +11,30 @@ class CubeTest(TestCase):
     
     # if valid cubelet faces are provided as input, a cube should be instantiated
     def test_cube_init_10010_ShouldInstantiateCubeForValidCubeCode(self):
-        cube = Cube('oboybbrrggrborywwroogggbygrooyyorbobygwwygbrwwwrywbywg')
+        
+        code = CubeCode('oboybbrrggrborywwroogggbygrooyyorbobygwwygbrwwwrywbywg')
+        
+        cube = Cube(code)
         self.assertIsInstance(cube, Cube)
     
     # if valid cubelet faces are provided as input, the NxNxN cube should have all N^3 cubelets present
     def test_cube_init_10020_ShouldHaveAllCubeletsToFillCube(self):
         
-        cube = Cube('obwgbrbyorgryrwgrrggwbgygrbbywboooorowyoyrgobygywwbyww')
+        code = CubeCode('obwgbrbyorgryrwgrrggwbgygrbbywboooorowyoyrgobygywwbyww')
         
+        cube = Cube(code)
         for i, j, k in itertools.product(*[range(Cube.SIZE)] * 3):
             cubelet = cube.cubelets[i, j, k]
             self.assertIsInstance(cubelet, Cubelet)
     
     ## __init__ - NEGATIVE TESTS
     
-    # supplying a non-string param should throw exception
-    def test_cube_init_20010_ShouldThrowExceptionForInvalidParamType(self):
+    # supplying invalid cube code should throw exception
+    def test_cube_init_20010_ShouldThrowExceptionForInvalidCubeCode(self):
         with self.assertRaises(Exception):
             Cube(2.3)
     
-    # supplying no param should throw exception
-    def test_cube_init_20020_ShouldThrowExceptionForNonSuppliedParam(self):
+    # supplying no cube code should throw exception
+    def test_cube_init_20020_ShouldThrowExceptionForNonSuppliedCubeCode(self):
         with self.assertRaises(Exception):
             Cube()
-            
-    # supplying an empty string param should throw exception
-    def test_cube_init_20030_ShouldThrowExceptionForEmptyStringParam(self):
-        faces = ''
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-            
-    # supplying a string param less than 54 chars long should throw exception
-    def test_cube_init_20040_ShouldThrowExceptionForStringParamLessThan54CharsLong(self):
-        faces = 'orgybowrrrrygrywrgggybgrwobywoooybbrwogyggywowbwwbboy'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-            
-    # supplying a string param more than 54 chars long should throw exception
-    def test_cube_init_20050_ShouldThrowExceptionForStringParamMoreThan54CharsLong(self):
-        faces = 'wowbbwrybryyrrbyygowwygoobowbgrgorgwgogbryggwbyrrbwoyow'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-    
-    # supplying a string param not over the alphabet [brgoyw] should throw exception
-    def test_cube_init_20060_ShouldThrowExceptionForStringParamContainingNonColorChars(self):
-        faces = 'gorbbgobbwgowrrwrbgwwygyyggr!rgowyybbrwwyrybgyyoowboor'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-    
-    # supplying a string param not containing every color code should throw exception
-    def test_cube_init_20070_ShouldThrowExceptionForStringParamNotContainingEveryColor(self):
-        faces = 'ggwobgrrbrwgorrwggwwoggbrgggbrwobbrwggorgobobggowwbogg'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-            
-    # supplying a string param with an uneven distribution of colors should throw exception
-    def test_cube_init_20080_ShouldThrowExceptionForStringParamWithUnevenColorDistribution(self):
-        faces = 'wobrbrrryyoowrwrggggyggwrrwgyroobobborwbyyggowwbowybyy'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
-            
-    # supplying a string param with non-unique center cubelet face colors should throw exception
-    def test_cube_init_20090_ShouldThrowExceptionForStringParamWithNonUniqueCenterFaceColors(self):
-        faces = 'gyyogroywgrygrorbwryyggbbwwbwowoboybrbgoywwooyggrwrbbr'
-        
-        with self.assertRaises(Exception):
-            Cube(faces)
