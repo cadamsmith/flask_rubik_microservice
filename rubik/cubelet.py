d05334123d74e44e18a4e4398ec1bf76c64a60cd
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-01 19:53:18 -0500

    Green Light |

--- rubik/cubelet.py
+++ rubik/cubelet.py
@@ -15,3 +15,5 @@ class Cubelet:
         # make sure that the param faces is a dictionary with CubeFace keys and CubeColor values
         assert (all(isinstance(face, CubeFace) for face in faces.keys()))
         assert (all(isinstance(color, CubeColor) for color in faces.values()))
+        
+        self.faces.update(faces)
