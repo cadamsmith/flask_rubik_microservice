37d74b3629ba33c148f89d48893d99b4552fc17f
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-01 19:04:51 -0500

    Green Light |

--- rubik/cubelet.py
+++ rubik/cubelet.py
@@ -1,5 +1,16 @@
 
+from rubik.cubeFace import CubeFace
+from rubik.cubeColor import CubeColor
+
 class Cubelet:
 
-    def __init__(self, sides):
-        pass
+    def __init__(self, faces = {}):
+        
+        # initialize all faces to no color
+        self.faces = list(map(
+            {s: None for s in CubeFace}
+        ))
+        
+        # make sure that the param faces is a dictionary with CubeFace keys and CubeColor values
+        assert (all(isinstance(face, CubeFace) for face in faces.keys()))
+        assert (all(isinstance(color, CubeColor) for color in faces.values())
