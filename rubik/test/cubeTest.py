6a6821fe4d5293bd20e996fa952fb27591c27356
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 05:29:09 -0500

    Red Light |

--- rubik/test/cubeTest.py
+++ rubik/test/cubeTest.py
@@ -10,3 +10,29 @@ class CubeTest(TestCase):
     def test_cube_init_20010_ShouldThrowExceptionForInvalidParamType(self):
         with self.assertRaises(Exception):
             Cube(2.3)
+    
+    # supplying no param should throw exception
+    def test_cube_init_20020_ShouldThrowExceptionForNonSuppliedParam(self):
+        with self.assertRaises(Exception):
+            Cube()
+            
+    # supplying an empty string param should throw exception
+    def test_cube_init_20030_ShouldThrowExceptionForEmptyStringParam(self):
+        faces = ''
+        
+        with self.assertRaises(Exception):
+            Cube(faces)
+            
+    # supplying an string param less than 54 chars long should throw exception
+    def test_cube_init_20040_ShouldThrowExceptionForStringParamLessThan54CharsLong(self):
+        faces = 'orgybowrrrrygrywrgggybgrwobywoooybbrwogyggywowbwwbboy'
+        
+        with self.assertRaises(Exception):
+            Cube(faces)
+            
+    # supplying an string param more than 54 chars long should throw exception
+    def test_cube_init_20050_ShouldThrowExceptionForStringParamMoreThan54CharsLong(self):
+        faces = 'wowbbwrybryyrrbyygowwygoobowbgrgorgwgogbryggwbyrrbwoyow'
+        
+        with self.assertRaises(Exception):
+            Cube(faces)
