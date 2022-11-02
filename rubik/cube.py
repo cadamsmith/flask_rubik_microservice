4176b9a161cfdec5ddbc5fac4bc009aed0a75db3
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 06:03:04 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -1,4 +1,5 @@
 
+import re
 from rubik.cubeColor import CubeColor
 
 class Cube:
@@ -21,6 +22,9 @@ class Cube:
         
         # make sure "cube code" is 54 chars long
         assert (len(faces) is 54)
+        
+        # make sure is over alphabet [brgoyw]
+        assert (not bool(re.search('[^brgoyw]', faces)))
         
         # make sure contains every color
         for color in list(CubeColor):
