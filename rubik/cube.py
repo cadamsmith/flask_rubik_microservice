a1d21fecbd5c9b3ac92a6a1f1103cd837c13b1a0
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 05:33:58 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -4,9 +4,17 @@ class Cube:
     '''
 
     def __init__(self, faces):
+        # make sure supplied param is either a string or dictionary
         assert (isinstance(faces, str) or isinstance(faces, dict))
         
         if isinstance(faces, str):
-            pass
+            self.__initFromCubeCode__(faces)
         elif isinstance(faces, dict):
             pass
+        
+    def __initFromCubeCode__(self, faces):
+        # make sure supplied param is a string
+        assert (isinstance(faces, str))
+        
+        # make sure "cube code" is 54 chars long
+        assert (len(str) is 54)
