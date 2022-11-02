e539393687aee6049c35682ac3155eddeea9c4ee
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-02 05:19:03 -0500

    Refactor |

--- rubik/cube.py
+++ rubik/cube.py
@@ -3,7 +3,10 @@ class Cube:
     Rubik's cube
     '''
 
-    def __init__(self):
-        pass
-
- 
+    def __init__(self, faces):
+        assert (isinstance(faces, str) or isinstance(faces, dict))
+        
+        if isinstance(faces, str):
+            pass
+        elif isinstance(faces, dict):
+            pass
