9f0f4bcc2690938758c1caaacb1a93b56c7d0e1f
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-06 11:06:23 -0600

    Green Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -12,12 +12,49 @@ class RotateTest(TestCase):
         self.assertIn('status', result)
         self.assertEqual(result['status'], rotate.ERROR_MISSING_CUBE)
     
-    # supplying invalid dir param should result in error status
-    def test_rotate_20020_ShouldErrorOnInvalidDirection(self):
+    # supplying non-string dir param should result in error status
+    def test_rotate_20020_ShouldErrorOnNonStringDir(self):
         result = rotate._rotate({
             'op': 'rotate',
             'cube': 'brobbowwybyobroobowbrggwwrybwygoygogwwbrygrgyrogywrryg',
-            'dir': 'p'
+            'dir': {
+                'a': 'b' 
+            }
+        })
+        
+        self.assertIn('status', result)
+        self.assertEqual(result['status'], rotate.ERROR_INVALID_DIR)
+        
+    # supplying empty string dir param should result in error status
+    def test_rotate_20021_ShouldErrorOnEmptyStringDir(self):
+        result = rotate._rotate({
+            'op': 'rotate',
+            'cube': 'brobbowwybyobroobowbrggwwrybwygoygogwwbrygrgyrogywrryg',
+            'dir': ''
+        })
+        
+        self.assertIn('status', result)
+        self.assertEqual(result['status'], rotate.ERROR_INVALID_DIR)
+        
+    # supplying string dir param with one character should result in error status if the character
+    # is an invalid rotational code
+    def test_rotate_20022_ShouldErrorOnMultipleCharDirWithAnyInvalidRotationalCodes(self):
+        result = rotate._rotate({
+            'op': 'rotate',
+            'cube': 'brobbowwybyobroobowbrggwwrybwygoygogwwbrygrgyrogywrryg',
+            'dir': 'P'
+        })
+        
+        self.assertIn('status', result)
+        self.assertEqual(result['status'], rotate.ERROR_INVALID_DIR)
+        
+    # supplying string dir param with multiple characters should result in error status
+    # if any of the characters are invalid rotational codes
+    def test_rotate_20023_ShouldErrorOnMultipleCharDirWithAnyInvalidRotationalCodes(self):
+        result = rotate._rotate({
+            'op': 'rotate',
+            'cube': 'brobbowwybyobroobowbrggwwrybwygoygogwwbrygrgyrogywrryg',
+            'dir': 'FfRrBbELlUuDd'
         })
         
         self.assertIn('status', result)
