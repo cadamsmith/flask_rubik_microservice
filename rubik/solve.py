0e40b29ddc87bc0f3e8cab0aec0b6fb78fb2b12c
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-07 18:42:17 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -1,9 +1,30 @@
 import rubik.cube as rubik
+from rubik.cubeCode import CubeCode
 
-def _solve(parms):
+ERROR_MISSING_CUBE = 'error: missing cube'
+ERROR_INVALID_CUBE = 'error: invalid cube'
+
+def _solve(params):
     """Return rotates needed to solve input cube"""
+    
+    # validate that 'cube' param exists
+    if 'cube' not in params:
+        return __missingCubeError__()
+    
+    cubeCodeText = params['cube']
+    
+    # validate that 'cube' param conforms to spec
+    if not CubeCode.isValid(cubeCodeText):
+        return __invalidCubeError__()
+    
     result = {}
-    encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
+    encodedCube = params.get('cube',None)       #STUB:  get "cube" parameter if present
     result['solution'] = 'FfRrBbLlUuDd'        #STUB:  example rotations
     result['status'] = 'ok'                     
     return result
+
+def __missingCubeError__():
+    return {'status': ERROR_MISSING_CUBE}
+
+def __invalidCubeError__():
+    return {'status': ERROR_INVALID_CUBE}
