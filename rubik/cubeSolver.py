e62b8d72a14e7963ab432955a1879a2fc6906bbc
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-07 20:29:55 -0600

    Green Light |

--- rubik/cubeSolver.py
+++ rubik/cubeSolver.py
@@ -1,5 +1,7 @@
 
 from rubik.cube import Cube
+from rubik.cubeFacePosition import CubeFacePosition
+from rubik.faceRotationDirection import FaceRotationDirection
 
 class CubeSolver():
     
@@ -9,4 +11,71 @@ class CubeSolver():
         self.cube = cube
         
     def solve(self):
-        return []
+        
+        directions = []
+        
+        directions = self.__transformFromUpDaisyToBottomCross(directions)
+        
+        return directions
+    
+    def __transformFromUpDaisyToBottomCross(self, directions):
+        
+        # front face
+        
+        (centerX, centerY, centerZ) = Cube.FACE_CENTER_CUBELET_COORDS[CubeFacePosition.FRONT]
+        aboveCenterCoord = (centerX, centerY - 1, centerZ)
+        
+        faceColor = self.cube.cubelets[(centerX, centerY, centerZ)].faces[CubeFacePosition.FRONT]
+        
+        while faceColor != self.cube.cubelets[aboveCenterCoord].faces[CubeFacePosition.FRONT]:
+            
+            direction = (CubeFacePosition.UP, FaceRotationDirection.CLOCKWISE)
+            
+            self.cube.rotateFace(direction)
+            directions.push(direction)
+            
+        # left face
+        
+        (centerX, centerY, centerZ) = Cube.FACE_CENTER_CUBELET_COORDS[CubeFacePosition.LEFT]
+        aboveCenterCoord = (centerX, centerY - 1, centerZ)
+        
+        faceColor = self.cube.cubelets[(centerX, centerY, centerZ)].faces[CubeFacePosition.LEFT]
+        
+        while faceColor != self.cube.cubelets[aboveCenterCoord].faces[CubeFacePosition.LEFT]:
+            
+            direction = (CubeFacePosition.UP, FaceRotationDirection.CLOCKWISE)
+            
+            self.cube.rotateFace(direction)
+            directions.push(direction)
+        
+        # back face
+        
+        (centerX, centerY, centerZ) = Cube.FACE_CENTER_CUBELET_COORDS[CubeFacePosition.BACK]
+        aboveCenterCoord = (centerX, centerY - 1, centerZ)
+        
+        faceColor = self.cube.cubelets[(centerX, centerY, centerZ)].faces[CubeFacePosition.BACK]
+        
+        while faceColor != self.cube.cubelets[aboveCenterCoord].faces[CubeFacePosition.BACK]:
+            
+            direction = (CubeFacePosition.UP, FaceRotationDirection.CLOCKWISE)
+            
+            self.cube.rotateFace(direction)
+            directions.push(direction)
+        
+        # right face
+        
+        (centerX, centerY, centerZ) = Cube.FACE_CENTER_CUBELET_COORDS[CubeFacePosition.RIGHT]
+        aboveCenterCoord = (centerX, centerY - 1, centerZ)
+        
+        faceColor = self.cube.cubelets[(centerX, centerY, centerZ)].faces[CubeFacePosition.RIGHT]
+        
+        while faceColor != self.cube.cubelets[aboveCenterCoord].faces[CubeFacePosition.RIGHT]:
+            
+            direction = (CubeFacePosition.UP, FaceRotationDirection.CLOCKWISE)
+            
+            self.cube.rotateFace(direction)
+            directions.push(direction)
+        
+        return directions
+        
+        
