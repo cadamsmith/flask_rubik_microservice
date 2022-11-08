86d66c8ced89525baaba1045f9a08d0b6cdd37f2
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-07 18:56:40 -0600

    Yellow Light |

--- rubik/test/cubeSolverTest.py
+++ rubik/test/cubeSolverTest.py
@@ -1,8 +1,20 @@
 
 from unittest import TestCase
 from rubik.cubeSolver import CubeSolver
+from rubik.cube import Cube
+from rubik.cubeCode import CubeCode
 
 class CubeSolverTest(TestCase):
+    
+    # init - POSITIVE TESTS
+    
+    # supplying valid cube should instantiate cube solver
+    def test_cubeSolver_init_10010_ShouldInstantiateCubeForValidCubeCode(self):
+        
+        cube = Cube(CubeCode('oboybbrrggrborywwroogggbygrooyyorbobygwwygbrwwwrywbywg'))
+        solver = CubeSolver(cube)
+        
+        self.assertIsInstance(solver, CubeSolver)
     
     # init - NEGATIVE TESTS
     
