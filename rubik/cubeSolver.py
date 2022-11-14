4bf64909100c3c06d7277f19e8e283b9a133107c
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-14 00:12:41 -0600

    Green Light |

--- rubik/cubeSolver.py
+++ rubik/cubeSolver.py
@@ -51,7 +51,7 @@ class CubeSolver():
         assert (isinstance(direction, FaceRotationDirection))
         
         self._cube.rotateFace(facePosition, direction)
-        self._solution.add((facePosition, direction))
+        self._solution.append((facePosition, direction))
         
     def getDirections(self):
         """ accessor for _solution field """
