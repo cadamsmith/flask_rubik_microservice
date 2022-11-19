204bd7a783bd9d80bb8150158b0f54b46fefc46b
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-18 23:09:36 -0600

    Refactor |

--- rubik/test/cubeRotationDirectionTest.py
+++ rubik/test/cubeRotationDirectionTest.py
@@ -5,23 +5,26 @@ from rubik.cubeRotationDirection import CubeRotationDirection
 
 class CubeRotationDirectionTest(TestCase):
     
-    # GENERAL TESTS
+    ''' CubeRotationDirection -- GENERAL TESTS '''
     
-    # there should be only 6 cube rotation directions (by nature of a cube)
     def test_cubeRotationDirection_00010_ShouldHaveExactlySixItems(self):
+        """ there should be only 6 cube rotation directions (by nature of a cube) """
+        
         expected = 6
         actual = len(list(CubeRotationDirection))
         
         self.assertEqual(expected, actual)
-        
-    # NEGATIVE TESTS
     
-    # supplying no face code should result in exception
+    ''' CubeRotationDirection -- NEGATIVE TESTS '''
+    
     def test_cubeRotationDirection_20010_ShouldThrowExceptionIfNoDirectionCodeSupplied(self):
+        """ supplying no face code should result in exception """
+        
         with self.assertRaises(Exception):
             CubeRotationDirection()
     
-    # supplying invalid face code should result in exception
     def test_cubeRotationDirection_20020_ShouldThrowExceptionIfInvalidDirectionCodeSupplied(self):
+        """ supplying invalid face code should result in exception """
+        
         with self.assertRaises(Exception):
             CubeRotationDirection('T')
