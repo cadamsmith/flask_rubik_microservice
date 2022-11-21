633ba68adb1dd868066da1eeb88adb3bce9fbf0e
cadamsmith <cadamsmith.dev@gmail.com>
2022-11-21 05:43:39 -0600

    Refactor |

--- rubik/sandbox/lineCounter.py
+++ rubik/sandbox/lineCounter.py
@@ -6,7 +6,7 @@ f = open("../" + fileName, "r")
 
 for l in f:
     l = l.strip()
-    if l and not l.startswith('#') and not l.startswith('"""'):
+    if l and not l.startswith('#') and not l.startswith('"""') and not l.startswith("'''"):
         count += 1
 
 print(count)
